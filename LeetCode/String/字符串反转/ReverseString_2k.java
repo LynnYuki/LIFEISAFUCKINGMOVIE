e5d2f9a43b01205afff14f9b2f7eb60ed6682985
LynnYuki <lynnyuki9527@163.com>
2020-12-06 20:43:56 +0800

    :)

--- LeetCode/String/字符串反转/ReverseString_2k.java
+++ LeetCode/String/字符串反转/ReverseString_2k.java
@@ -1,5 +1,5 @@
 /**
- * 反转2k字符串
+ * leetcode 541反转2k字符串
  */
 
 class Solution {
@@ -8,7 +8,7 @@ class Solution {
         char[] ch = s.toCharArray();
         for (int init = 0; init < n; init += 2*k) {
             int start = init;
-            //判断的关键 start + k - 1
+            //判断的关键 start + k - 1 是否越界 越界则说明剩下的数组元素不够
             int end = Math.min(start + k - 1,n - 1);
             while (start < end) {
                 char temp = ch[start];
