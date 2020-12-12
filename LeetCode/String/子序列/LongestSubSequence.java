436f68c72f97b85da5bf9e6d1e50a2ca2dd40b5c
lynnyuki9527 <lynnyuki9527@163.com>
2020-12-12 09:13:05 +0800

    leetcode 521

---
+++ LeetCode/String/子序列/LongestSubSequence.java
@@ -0,0 +1,11 @@
+/**
+ * leetcode 521 最长特殊序列
+ */
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return Math.max(a.length(),b.length());
+    }
+}
