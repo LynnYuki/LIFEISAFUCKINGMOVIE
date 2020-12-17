826b72409e892a2b5242d367594b952a5f8ae7e1
LynnYuki <lynnyuki9527@163.com>
2020-12-17 09:57:31 +0800

    leetcode 141

--- LeetCode/List/链表是否有环/HasCycleNode.java
+++ LeetCode/List/链表是否有环/HasCycleNode.java
@@ -22,4 +22,27 @@ public class Solution {
         }
         return false;
     }
+    
+    //快慢指针
+     public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+        //快指针指向头节点的直接后继节点
+        ListNode fast = head.next;
+        //慢指针指向头结点
+        ListNode slow = head;
+        while (slow != fast) {
+            //如果没环则fast会指向null
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            //移动一步
+            slow = slow.next;
+            //移动两部
+            fast = fast.next.next;
+        }
+        return true;
+    }
+    
 }
