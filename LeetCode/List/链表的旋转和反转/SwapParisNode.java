31a1f214916629a03957c4637ade94904afefe8a
LynnYuki <lynnyuki9527@163.com>
2020-12-18 10:32:14 +0800

    leetcode 24 recurse

--- LeetCode/List/链表的旋转和反转/SwapParisNode.java
+++ LeetCode/List/链表的旋转和反转/SwapParisNode.java
@@ -27,4 +27,27 @@ class Solution {
          }
          return sentinel.next;
     }
+
+    //递归
+    //其中我们应该关心的主要有三点：
+
+    // 返回值
+    // 调用单元做了什么
+    // 终止条件
+    // 在本题中：
+    // 返回值：交换完成的子链表
+    // 调用单元：设需要交换的两个点为 head 和 next，head 连接后面交换完成的子链表，next 连接 head，完成交换
+    // 终止条件：head 为空指针或者 next 为空指针，也就是当前无节点或者只有一个节点，无法进行交换
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //next 为头结点的直接后继节点
+        ListNode next = head.next;
+        //head连接已经完成交换的子链表
+        head.next = swapPairs(next.next);
+        //next指向head
+        next.next = head;
+        return next;
+    }
 }
