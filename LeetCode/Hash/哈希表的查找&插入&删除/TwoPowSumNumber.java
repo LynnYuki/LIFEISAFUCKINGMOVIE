a88928ce90e2b54818d67c25839cc3145ca0820c
lynnyuki <lynnyuki9527@163.com>
2020-12-21 10:20:52 +0800

    leetcode 633

---
+++ LeetCode/Hash/哈希表的查找&插入&删除/TwoPowSumNumber.java
@@ -0,0 +1,15 @@
+
+/**
+ * leetcode 633 平方数之和
+ */
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if(b == (int) b) {
+                return true;
+            }      
+        }
+        return false;
+    }
+}
