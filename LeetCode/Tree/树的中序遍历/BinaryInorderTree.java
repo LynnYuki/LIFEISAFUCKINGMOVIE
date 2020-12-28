87d045b78079172586103994d16c76398e609abe
lynnyuki <lynnyuki9527@163.com>
2020-12-28 10:05:13 +0800

    leetcode 94

---
+++ LeetCode/Tree/树的中序遍历/BinaryInorderTree.java
@@ -0,0 +1,48 @@
+/**
+ * leetcode 94 树的中序遍历
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //递归
+     List<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        inorderTraversal(root.left);
+        res.add(root.val);
+        inorderTraversal(root.right);
+        return res;
+
+    }
+    //迭代
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
