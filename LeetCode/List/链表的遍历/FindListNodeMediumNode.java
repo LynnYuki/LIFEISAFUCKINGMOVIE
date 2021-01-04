855dd5ba7fdca57b2b670475138d6d255a16f7c7
LynnYuki <lynnyuki9527@163.com>
2021-01-04 22:07:58 +0800

    leetcode 806

---
+++ LeetCode/List/链表的遍历/FindListNodeMediumNode.java
@@ -0,0 +1,24 @@
+/**
+ *  leetcode 876. 链表的中间结点
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  //快慢指针
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
